e153279caa016c935ce26580539d21bcd02dc2d0
1MarcosAquino <1marcosaquino@gmail.com>
2024-03-06 20:10:12 -0300

    Container alterado

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,6 +1,10 @@
-// const URL_BASE = 'https://api.github.com/users/1MarcosAquino/repos';
-const URL_BASE = 'http://127.0.0.1:5500/db.json';
-export const api = async (url = URL_BASE) => {
+let baseUrl = 'https://api.github.com/users/1MarcosAquino/repos';
+
+if (location.hostname == '127.0.0.1' || location.hostname == 'localhost') {
+    baseUrl = 'http://127.0.0.1:5500/database.json';
+}
+
+export const api = async (url = baseUrl) => {
     return await fetch(url)
         .then((res) => res.json())
         .catch((res) => console.error(res));
