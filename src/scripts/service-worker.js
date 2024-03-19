bc1542150faa8515060f61ff7b38889a9e107c86
1MarcosAquino <1marcosaquino@gmail.com>
2024-03-19 16:28:28 -0300

    TESTE-2

--- src/scripts/service-worker.js
+++ src/scripts/service-worker.js
@@ -1,15 +1,6 @@
 const CACHE_NAME = 'portfolio-v1';
 const urlsToCache = ['./', './index.html', './public/icon.png'];
 
-// self.addEventListener('install', function (event) {
-//     event.waitUntil(
-//         caches.open(CACHE_NAME).then(function (cache) {
-//             console.log('Opened cache');
-//             return cache.addAll(urlsToCache);
-//         })
-//     );
-// });
-
 self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
